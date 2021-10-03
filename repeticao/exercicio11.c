76ad9db17d891e314899a693eb7de796a7121f60
Heitor Silva Avila <87899649+h3itoravila@users.noreply.github.com>
2021-10-02 22:22:50 -0300

    Create exercicio11.c

---
+++ repeticao/exercicio11.c
@@ -0,0 +1,24 @@
+/* Faça um programa que recebe lê n conceitos de alunos (n é fornecido no início do
+programa) e conta quantos conceitos de cada tipo existem. Os conceitos possíveis são: A, B,
+C, D, E e podem ser fornecidos com letras maiúsculas ou minúsculas. */
+#include <stdio.h>
+int main(void) {
+    int numeroDeConceitos, conceitosLidos = 0, qtdeA = 0, qtdeB = 0, qtdeC = 0, qtdeD = 0, qtdeE = 0;
+    char leituraDoConceito;
+    printf("Informe quantos conceitos deseja informar: \n> ");
+    scanf("%d", &numeroDeConceitos);
+    while (conceitosLidos < numeroDeConceitos) {
+        printf("Informe o conceito: \n> ");
+        // Limpeza do buffer
+        setbuf(stdin,NULL);
+        scanf(" %c", &leituraDoConceito);
+        //fgets(leituraDoConceito, 1, stdin);
+        if (leituraDoConceito == 'a' || leituraDoConceito == 'A') {qtdeA++;}
+        if (leituraDoConceito == 'b' || leituraDoConceito == 'B') {qtdeB++;}
+        if (leituraDoConceito == 'c' || leituraDoConceito == 'C') {qtdeC++;}
+        if (leituraDoConceito == 'd' || leituraDoConceito == 'D') {qtdeD++;}
+        if (leituraDoConceito == 'e' || leituraDoConceito == 'E') {qtdeE++;}
+        conceitosLidos++; }
+    printf("Total de conceitos:\n");
+    printf("A: %d \nB: %d \nC: %d \nD: %d \nE: %d \n", qtdeA, qtdeB, qtdeC, qtdeD, qtdeE);
+    return 0; }
